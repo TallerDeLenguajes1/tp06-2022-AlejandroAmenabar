1170965c5e274c85040b892f4854751a1a750d9d
Alejandro Amenabar <alegymnas@gmail.com>
2022-05-24 16:58:25 -0300

    punto 2 avance

--- TP6/Program.cs
+++ TP6/Program.cs
@@ -1,4 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+Empleado empleado = new Empleado();
 
+void cargarEmpleado(Empleado empleado)
+{
+    Console.WriteLine("Ingrese Nombre y Apellido ");
+    empleado.Nombre = Console.ReadLine();
+
+    empleado.Apellido  = Console.ReadLine();
+
+    empleado.fechaNacimiento = new DateTime(1990,3,14); //hacer random
+
+    Console.WriteLine("Estado Civil? S- soltero, C-casado");
+    empleado.estadoCivil = Convert.ToChar(Console.ReadLine());
+
+    Console.WriteLine("Genero? H-Hombre M-Mujer O- Otro");
+    empleado.genero = Convert.ToChar(Console.ReadLine());
+
+    empleado.fechaIngreso = new DateTime(2012,12,12); //hacer random
+
+    Console.WriteLine("Ingrese su sueldo");
+    empleado.sueldo = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Cual es su cargo en la empresa? 1-Auxiliar, 2-Administrativo, 3-Ingeniero, 4-Especialista, 5-Investigador");
+    empleado.cargo = cargos.Administrativo; //swithc que recba un numero y de ahi hacer cada caso 
+
+}
+
+int antiguedad = empleado.Antiguedad(empleado.fechaIngreso);
+Console.WriteLine("El empleado tiene:" +antiguedad+ "de antiguedad");
+
+//para asignarlo cargos cargoEmpleado = cargos.Especialista;
--- TP6/empleados.cs
+++ TP6/empleados.cs
@@ -1,3 +1,11 @@
+public enum cargos
+{ 
+    Auxiliar=1,
+    Administrativo=2,
+    Ingeniero=3,
+    Especialista=4,
+    Investigador=5
+}
 public class Empleado
 {
     public DateTime fechaNacimiento;
@@ -5,17 +13,15 @@ public class Empleado
     public char genero;
     public DateTime fechaIngreso;
     public double sueldo;
-
+    public cargos cargo;
     public string Nombre { get; set; }
     public string Apellido { get; set; }
 
-    public enum cargos
-    { 
-        Auxiliar,
-        Administrativo,
-        Ingeniero,
-        Especialista,
-        Investigador
+    
+    public int Antiguedad(DateTime fechaIngreso)
+    {
+        DateTime Actual = DateTime.Today;
+        int añosAntiguedad = Convert.ToInt32(Actual.Year - fechaIngreso.Year);
+        return añosAntiguedad;
     }
-    //para asignarlo cargos cargoEmpleado = cargos.Especialista;
 }
